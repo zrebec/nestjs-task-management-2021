99cfeef9c124546fd5e0a3b3c0f822c09652aeb4
Martin Fox Bencik <zrebec@zrebec.sk>
2023-06-10 22:54:36 +0200

    feat: forbidnonwhitelisted

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,16 @@ async function bootstrap() {
 
   // Using global validation pipes for DTO's
   app.useGlobalPipes(new ValidationPipe({
-    // If this is set to true, only DTO's props will be accepeted
+    // If whitelist is set to true, only DTO's props will be accepeted
     // in the request (e.g. in body).
+    // If forbidNonWhitelisted is set to true, any props which is out of
+    // DTO's will be forbidden and we get bad request response.
+    // This could be usesul for some injection protect
     // But in course they are not using this yet.
     // After couse, we can change it to true and test all functions
     // of better create tests 
     whitelist: false,
+    forbidNonWhitelisted: false,
   }));
   await app.listen(3000);
 }
