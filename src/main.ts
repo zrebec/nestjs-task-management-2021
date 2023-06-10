2d57c1351b9824ee42db683330152ea28274e81d
Martin Fox Bencik <zrebec@zrebec.sk>
2023-06-10 22:49:45 +0200

    feat: new global validation pipes knowns

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Using global validation pipes for DTO's
+  app.useGlobalPipes(new ValidationPipe({
+    // If this is set to true, only DTO's props will be accepeted
+    // in the request (e.g. in body).
+    // But in course they are not using this yet.
+    // After couse, we can change it to true and test all functions
+    // of better create tests 
+    whitelist: false,
+  }));
   await app.listen(3000);
 }
 bootstrap();
